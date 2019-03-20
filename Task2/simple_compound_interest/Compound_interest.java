be1083324371ffb07b3bb7d6e2bcafcc4e666b48
Adityavce007 <aditya.abhi123@gmail.com>
2019-03-20 23:03:45 +0530

    second

---
+++ Task2/simple_compound_interest/Compound_interest.java
@@ -0,0 +1,11 @@
+package simple_compound_interest;
+/**
+ * 
+ * @author Aditya Abhiram
+ * This program determines the compound interest for the given inputs.
+ */
+public class Compound_interest extends Interest {
+	public double interest(){
+		return principal*Math.pow((1+rate_of_interest), years)-principal;
+	}
+}
---
+++ Task2/simple_compound_interest/Interest.java
@@ -0,0 +1,12 @@
+package simple_compound_interest;
+/**
+ * 
+ * @author Aditya Abhiram
+ * This is an abstract class which can be extended by various classes.
+ *
+ */
+public abstract class Interest {
+	abstract double interest();
+	double principal,rate_of_interest,years;
+	
+}
