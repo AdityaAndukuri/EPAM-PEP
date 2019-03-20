9f5c9c2a9c4a411669ed280ecd403247b4ee4347
Adityavce007 <aditya.abhi123@gmail.com>
2019-03-20 21:03:08 +0530

    first

---
+++ Task1/First_Task.java
@@ -0,0 +1,13 @@
+package Task1;
+/**
+ * 
+ * @author Aditya Abhiram
+ * Hello World!
+ *
+ */
+public class First_Task {
+	public static void main(String args[])
+	{
+	System.out.println("Hello World!");
+	}
+}
