21dc5ef4698d09c2cb87952b8929715a914f44a9
Adityavce007 <aditya.abhi123@gmail.com>
2019-04-03 06:55:24 +0530

    Fourth

--- Task4/Self_Drive.java
+++ Task4/Self_Drive.java
@@ -5,24 +5,23 @@ public class Self_Drive {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
+        int n = sc.nextInt(),i,x,y;
+        long count = 0;
+        for (i = 0; i < n; i++) {
             adj.add(new TreeSet<>());
         }
-        for (int i = 0; i < n - 1; i++) {
-            int x = sc.nextInt() - 1;
-            int y = sc.nextInt() - 1;
+        for (i = 0; i < n - 1; i++) {
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
             adj.get(x).add(y);
             adj.get(y).add(x);
         }
         HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; ++i) {
+        for (i = 0; i < n; ++i) {
             set.add(i);
         }
-        long count = 0;
-        for (int i = 2; i <= n && !set.isEmpty(); i++) {
-            count += set.size();
+        for (i = 2; i <= n && !set.isEmpty(); i++) {
+            count = count+set.size();
             HashSet<Integer> nextset = new HashSet<>();
             for (Integer ele : set) {
                 if (ele + i - 1 < n && isConnected(ele + i - 1, ele, ele + i - 2)) {
@@ -34,7 +33,7 @@ public class Self_Drive {
             }
             set = nextset;
         }
-        count += set.size();
+        count = count+set.size();
         System.out.println(count);
     }
     static ArrayList<TreeSet<Integer>> adj = new ArrayList<>();
